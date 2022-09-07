9db79bf8bf44c81788213e6eca4ff007634ceec5
sylvain <yvonneau.sylvain@gmail.com>
2022-09-07 16:22:10 +0200

    test position initial vert

---
+++ src/Rover.ts
@@ -0,0 +1,7 @@
+export class Rover {
+  constructor(x: number, y: number) {}
+
+  displayPosition(): [number, number, string] {
+    return [0, 0, "N"];
+  }
+}
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,15 @@
 import { doTheThing } from ".";
+import { Rover } from "./Rover";
 
-describe("Test of the test framework", function() {
-  it("Should pass", function() {
+describe("Test of the test framework", function () {
+  it("Should pass", function () {
     expect(doTheThing()).toEqual(true);
+  });
+});
+
+describe("Test the position of the rover", function () {
+  it("Should display base position when the rover is initialized", function () {
+    const rover = new Rover(0, 0);
+    expect(rover.displayPosition()).toEqual([0, 0, "N"]);
   });
 });
